d47732d9ab5ee93ac53e2d230878fa9a187c6e40
kotasatyaveni2023-wq <kotasatyaveni2023@gmail.com>
2026-06-01 06:21:58 +0000

    Sync LeetCode submission Runtime - 3814 ms (8.94%), Memory - 19.7 MB (57.71%)

---
+++ 0387-first-unique-character-in-a-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for ch in s:
+            if s.count(ch)==1:
+                return s.index(ch)
+
+        return -1
+
+        

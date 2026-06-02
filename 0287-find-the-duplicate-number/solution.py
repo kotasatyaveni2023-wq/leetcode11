648ef43c18a246837ae483bf073201640746b3e4
kotasatyaveni2023-wq <kotasatyaveni2023@gmail.com>
2026-06-02 16:02:41 +0000

    Sync LeetCode submission Runtime - 34 ms (28.83%), Memory - 40.8 MB (6.67%)

---
+++ 0287-find-the-duplicate-number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        freq={}
+        for num in nums:
+            if num in freq:
+                freq[num]+=1
+            else:
+                freq[num]=1
+        for k,v in freq.items():
+            if v>1:
+                return k
+        

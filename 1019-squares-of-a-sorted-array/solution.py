aea2be5d738cb4ab239df5953743ed52ac04d0b6
kotasatyaveni2023-wq <kotasatyaveni2023@gmail.com>
2026-06-25 04:42:22 +0000

    Sync LeetCode submission Runtime - 3 ms (94.72%), Memory - 21.2 MB (28.43%)

---
+++ 1019-squares-of-a-sorted-array/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result=[num**2 for num in nums]
+        result.sort()
+        return result
+       
+
+        
+        
